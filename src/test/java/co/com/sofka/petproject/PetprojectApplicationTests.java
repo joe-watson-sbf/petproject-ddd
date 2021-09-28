f5d9cc982c6bedbe66e96145c6870d0ecd8d3f76
Joe Watson SBF <joewatson40@hotmail.com>
2021-09-28 18:47:49 -0500

    first commit

---
+++ src/test/java/co/com/sofka/petproject/PetprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
