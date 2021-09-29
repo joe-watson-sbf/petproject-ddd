64fea7e4be9b945f1aa71c6cee20afb894ad09a1
Joe Watson SBF <joewatson40@hotmail.com>
2021-09-29 02:38:59 -0500

    More attribut to Address Classe

--- src/main/java/co/com/sofka/petproject/domain/customer/value/Address.java
+++ src/main/java/co/com/sofka/petproject/domain/customer/value/Address.java
@@ -5,14 +5,24 @@ import co.com.sofka.domain.generic.ValueObject;
 import java.util.Objects;
 
 public class Address implements ValueObject<String> {
-    private final String value;
 
-    public Address(String value) {
-        this.value = Objects.requireNonNull(value.trim(), "Address cannot be null!!!");
+    private final String contry;
+    private final String state;
+    private final String street;
+    private final String city;
+    private final String zipCode;
+
+    public Address(String contry, String state, String street, String city, String zipCode) {
+        this.contry = Objects.requireNonNull(contry.trim(), "Country requiered!!!");
+        this.state = Objects.requireNonNull(state.trim(), "State requiered!!!");
+        this.street = Objects.requireNonNull(street.trim(), "Street requiered!!!");
+        this.city = Objects.requireNonNull(city.trim(), "City requiered!!!");
+        this.zipCode = Objects.requireNonNull(zipCode.trim(), "Zip Code requiered!!!");
     }
 
     @Override
     public String value() {
-        return value;
+        return this.street + ", "+ this.city + ", " +
+                this.state + ", " + this.contry + ", " + this.zipCode;
     }
 }
